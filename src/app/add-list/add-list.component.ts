8d51a4009f73064368632d6c75f04e01d31f1091
williamrmo <williamrmo84@gmail.com>
2019-04-20 20:35:29 -0600

    First add-list

---
+++ src/app/add-list/add-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-list',
+  templateUrl: './add-list.component.html',
+  styleUrls: ['./add-list.component.sass']
+})
+export class AddListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { AddListComponent } from './add-list/add-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    AddListComponent
   ],
   imports: [
     BrowserModule,
